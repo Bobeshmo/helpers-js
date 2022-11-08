ea862b4487e382328de648e68065dc01ba5816b0
Christopher <tchaikovsky.md@gmail.com>
2022-11-08 11:41:24 +0200

    Update Get Search Params method

--- lib/getSearchParams.ts
+++ lib/getSearchParams.ts
@@ -1,8 +1,12 @@
 function getSearchParams(search: string = location.search) {
     const params = search.substring(1);
 
+    const replaced = params
+        .replace(/&/g, '","')
+        .replace(/=/g, '":"');
+
     return JSON.parse(
-        '{"' + params.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
+        '{"' + replaced + '"}',
         function (key, value) {
             return key === "" ? value : decodeURIComponent(value);
         }
